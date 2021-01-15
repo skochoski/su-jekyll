8d94cf576782896482e3718e08a0b5f428462f7c
skochoski <hadzikocoski1@yahoo.com>
2021-01-15 14:24:44 +0200

    updated sass

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ const sass = require('gulp-sass');
 const cp = require('child_process');
 
 gulp.task('css', () => {
-    return gulp.src('_assets/css/**/*.css')
-        // .pipe(sass().on('error', sass.logError))
+    return gulp.src('_assets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./docs/css/'))
         .pipe(browserSync.stream({ match: '**/*.css' }));
@@ -30,7 +30,7 @@ gulp.task('watch', () => {
         }
     });
 
-    gulp.watch('_assets/css/**/*.css', gulp.series('css'));
+    gulp.watch('_assets/scss/**/*.scss', gulp.series('css'));
 
     gulp.watch(
         [
